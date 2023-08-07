ba43f07b3f13a5f10e2c6091d1183799bcc47d8c
Piyathida San-aoun <Piyathida_s101@hotmail.com>
2023-08-07 09:59:43 +0700

    update

---
+++ upload_results.ps1
@@ -0,0 +1,8 @@
+trcli -y `
+   -h "https://piyathida.testrail.io" `
+   --project "API_Test" `
+   --u "Piyathida.sanaoun01@gmail.com" `
+   --p "Zxcv@121416" `
+   parse_junit `
+   --title "RF Automated Tests" `
+   -f "junit-report.xml"
